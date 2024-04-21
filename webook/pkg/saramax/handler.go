5066fd4e7a10d9b59bfd95ee962d7fbed451e3f9
Hugo Lefan ZHANG <hugo.lefan.zhang@gmail.com>
2024-04-21 15:33:20 +0200

    add prometheus to record sarama consume metric

--- webook/pkg/saramax/handler.go
+++ webook/pkg/saramax/handler.go
@@ -2,20 +2,30 @@ package saramax
 
 import (
 	"encoding/json"
+	"time"
 	"webook/pkg/logger"
 
 	"github.com/IBM/sarama"
+	"github.com/prometheus/client_golang/prometheus"
 )
 
 type Handler[T any] struct {
-	l  logger.LoggerV1
-	fn func(msg *sarama.ConsumerMessage, evt T) error
+	l      logger.LoggerV1
+	fn     func(msg *sarama.ConsumerMessage, evt T) error
+	vector *prometheus.SummaryVec
 }
 
 func NewHandler[T any](l logger.LoggerV1, fn func(msg *sarama.ConsumerMessage, t T) error) *Handler[T] {
+	vector := prometheus.NewSummaryVec(prometheus.SummaryOpts{
+		Namespace: "riiceball",
+		Subsystem: "webook",
+		Name:      "sarama 消费",
+		Help:      "统计 sarama 消费的耗时",
+	}, []string{"topic"})
 	return &Handler[T]{
-		l:  l,
-		fn: fn,
+		l:      l,
+		fn:     fn,
+		vector: vector,
 	}
 }
 
@@ -30,6 +40,8 @@ func (h *Handler[T]) Cleanup(session sarama.ConsumerGroupSession) error {
 func (h *Handler[T]) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
 	msgs := claim.Messages()
 	for msg := range msgs {
+		// 计算消费耗时
+		start := time.Now()
 		var t T
 		err := json.Unmarshal(msg.Value, &t)
 		if err != nil {
@@ -49,6 +61,9 @@ func (h *Handler[T]) ConsumeClaim(session sarama.ConsumerGroupSession, claim sar
 				logger.Error(err))
 			continue
 		}
+		duration := time.Since(start).Milliseconds()
+		h.vector.WithLabelValues(msg.Topic).
+			Observe(float64(duration))
 		session.MarkMessage(msg, "")
 	}
 	return nil
